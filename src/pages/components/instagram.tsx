eb155b567aad73d12173fe1e60c9e7fd01a631e9
Emilia Repo <emiliarepo@icloud.com>
2023-10-23 19:43:18 +0300

    async script

--- src/pages/components/instagram.tsx
+++ src/pages/components/instagram.tsx
@@ -1,9 +1,8 @@
 export default function Instagram() {
   return (
     <>
-      <script src="https://w.behold.so/widget.js" type="module"></script>
+      <script async src="https://w.behold.so/widget.js" type="module"></script>
       <figure data-behold-id="XMcA23wZ7XkMhdwTDS3h"></figure>
     </>
   );
 }
-
